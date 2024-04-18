f21ed0acfb6f9452fc4cd6f615f445956226fbe9
Will Cumbicus <wilmanvc93@gmail.com>
2024-04-18 21:34:47 +0200

    feat: implement error interceptor

---
+++ componentes-personalizados/src/app/interceptors/error-handler.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let errorMessage = '';
+
+      if (error.error instanceof ErrorEvent) {
+        errorMessage = `Error: ${error.error.message}`;
+      } else {
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      }
+      console.error(errorMessage);
+
+      return throwError(() => errorMessage);
+    })
+  );
+};
