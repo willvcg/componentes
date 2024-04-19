1d1a3a6572614726162d8e02539881d26d9bff63
Will Cumbicus <wilmanvc93@gmail.com>
2024-04-19 18:11:26 +0200

    feat: add paginated

--- componentes-personalizados/src/app/componentes/table/table.component.ts
+++ componentes-personalizados/src/app/componentes/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, input, output, signal } from '@angular/core';
 import { SafeValue } from '@angular/platform-browser';
-import { ButtonComponent } from "../button/button.component";
+import { PokemonList } from '../../models/pokemon';
+import { ButtonComponent } from '../button/button.component';
 
 export interface Usuario {
   nombre: string;
@@ -13,79 +14,114 @@ export interface HeadTable {
   fieldName: string;
 }
 
-export type CustomItem<T = Options> = Record<Extract<keyof T, string>, any> & RecordOptions
+export type CustomItem<T = Options> = Record<Extract<keyof T, string>, any> &
+  RecordOptions;
 
-export type Options = Record<string , string>
+export type Options = Record<string, string>;
 
 export type RecordOptions = {
   /** Opciones para el collapse */
-  collapse?: CollapseOptions
+  collapse?: CollapseOptions;
   /** Clase CSS personalizada de la fila */
-  rowClass?: string
+  rowClass?: string;
   /** Contenido HTML personalizado */
-  innerHtml?: string
+  innerHtml?: string;
   /** Segundo contendido HTML personalizado */
-  innerHtml2?: string | SafeValue
+  innerHtml2?: string | SafeValue;
   /** Clase personalizada para cada columna de cada registro. Por orden */
-  classes?: string[]
-}
+  classes?: string[];
+};
 
 export type CollapseOptions = {
   /** HTML */
-  inner: string,
-  class?: string,
-}
+  inner: string;
+  class?: string;
+};
 
 export type ButtonOptions = {
-  title: string,
-  class?: string,
-  tooltip?: string,
-  callback: (ev: any) => void
-}
+  title: string;
+  class?: string;
+  tooltip?: string;
+  callback: (ev: any) => void;
+};
 export type ButtonsOptions = {
-  buttons: ButtonOptions[],
-  class?: string
-}
+  buttons: ButtonOptions[];
+  class?: string;
+};
 
 export type SelectOptions = {
   /** Key en el *data* que es desplegable *select* */
-  selectedKey: string
+  selectedKey: string;
   /** Key en el *data* que contiene los *values* del desplegable @see {@link Selected}*/
-  selectKeyValues: string,
+  selectKeyValues: string;
   /** Clase para el select */
-  class?: string,
-  disabled?: boolean,
-  callback?: (ev: SelectedChange) => void
-}
+  class?: string;
+  disabled?: boolean;
+  callback?: (ev: SelectedChange) => void;
+};
 
 export type SelectedChange = {
-  item: CustomItem
-  selected: string
-}
+  item: CustomItem;
+  selected: string;
+};
+
+export const defaultTablePaginationParams = {
+  limit: 10,
+  offset: 0,
+};
 
 @Component({
-    selector: 'app-table',
-    standalone: true,
-    templateUrl: './table.component.html',
-    styleUrl: './table.component.css',
-    imports: [ButtonComponent]
+  selector: 'app-table',
+  standalone: true,
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.css',
+  imports: [ButtonComponent],
 })
 export class TableComponent {
+  headArray = input.required<HeadTable[]>();
+  gridArray = input.required<CustomItem[] | PokemonList>();
+  buttonsOptions = input<ButtonsOptions>();
+  selectOptions = input<SelectOptions>();
+  showPaginator = input<boolean>(true);
+  prevPage = output<string>();
+  nextPage = output<string>();
+  dataArray = computed((): CustomItem[] => {
+    const gridArray = this.gridArray();
+    return Array.isArray(gridArray) ? gridArray : gridArray.results;
+  });
 
-  headArray = input.required<HeadTable[]>()
-  gridArray = input.required<CustomItem[]>()
-  buttonsOptions = input<ButtonsOptions>()
-  selectOptions = input<SelectOptions>()
+  // Propertys for paginator
+  currentPage = signal<number>(1);
+  itemsPerPage = signal<number>(10);
+  totalPages = signal<number>(0);
 
   protected onButtonsOptionsClick(item: CustomItem, button: ButtonOptions) {
-    item && button.callback?.(item)
+    item && button.callback?.(item);
   }
 
-  protected onSelectOptionsClick(item: CustomItem, select: SelectOptions, event: Event){
-    const selected = (event.target as HTMLSelectElement).value
-    item && select.callback?.({item, selected})
+  protected onSelectOptionsClick(
+    item: CustomItem,
+    select: SelectOptions,
+    event: Event
+  ) {
+    const selected = (event.target as HTMLSelectElement).value;
+    item && select.callback?.({ item, selected });
   }
-  
+
+  protected onNextChange(page: number) {
+    // this.currentPage.set(page);
+    const nextPage = this.gridArray() as PokemonList;
+    if (!nextPage.next) return;
+    this.nextPage.emit(nextPage.next);
+  }
+
+  protected onPrevChange(page: number) {
+    // this.currentPage.set(page);
+    const prevPage = this.gridArray() as PokemonList;
+    if (!prevPage.previous) return;
+    this.prevPage.emit(prevPage.previous);
+  }
+
   /** Opciones pasadas e options (key: value) */
   // protected op = computed(()=> {
   //   const headArray = this.headArray()
---
+++ componentes-personalizados/src/app/models/paginated.ts
@@ -0,0 +1,7 @@
+export interface PaginationParams {
+  sort?: string;
+  direction?: string;
+  limit?: number;
+  offset?: number;
+  filter?: string;
+}
