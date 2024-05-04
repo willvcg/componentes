0b669df5c1a9e782819d0bea82af1e20f186ce3f
Will Cumbicus <wilmanvc93@gmail.com>
2024-05-04 11:47:33 +0200

    chore: add select with ngModel and Form

--- componentes-personalizados/src/app/pages/selects-page/selects-page.component.ts
+++ componentes-personalizados/src/app/pages/selects-page/selects-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import {
   SelectComponent,
   Selected,
@@ -9,9 +16,10 @@ import {
   standalone: true,
   templateUrl: './selects-page.component.html',
   styleUrl: './selects-page.component.css',
-  imports: [SelectComponent],
+  imports: [SelectComponent, FormsModule, ReactiveFormsModule],
 })
 export class SelectsPageComponent {
+  private formBuilder: FormBuilder = inject(FormBuilder);
   /** Array de los tipos de documentos */
   tipoDocumentos = {
     '0': 'Selecciona un tipo de documento',
@@ -22,6 +30,25 @@ export class SelectsPageComponent {
   };
   /** Documento seleccionado */
   documento: Selected<typeof this.tipoDocumentos> = 'NIF';
+  myForm: FormGroup = this.formBuilder.group({
+    documento: ['', Validators.required],
+  });
+
+  /** Array de los tipos de plantas */
+  tipoPlantas = {
+    '0': 'Selecciona un tipo de planta',
+    INTERIOR: 'Interior',
+    EXTERIOR: 'Exterior',
+  };
+  /** Planta seleccionada */
+  planta: Selected<typeof this.tipoPlantas> = 'INTERIOR';
+  plantaNgModel?: string;
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      const selectedDocumento = this.myForm.value.documento;
+    }
+  }
 
   protected selectedChange(event: any) {
     console.log('event', event);
